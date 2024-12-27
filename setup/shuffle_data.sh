719945045164bdb967c0e8c9cf8bd8c2adb67763
miraumlauf <bi235@uni-heidelberg.de>
2024-12-27 12:32:49 +0100

    Rename data shuffle submit

--- setup/shuffle_data.sh
+++ setup/shuffle_data.sh
@@ -12,7 +12,9 @@
 
 source activate lingua_241105
 
-python setup/shuffle_babylm.py babylm_100M_clean_json 32 --data_dir ./babylm_data_json --seed 42
+# python setup/shuffle_babylm.py babylm_100M_clean_json 4 --data_dir ./babylm_data_json --seed 42
+python setup/shuffle_babylm.py babylm_dev_clean_json 4 --data_dir ./babylm_data_json --seed 42
+
 
 # python setup/prepare_reduced_babylm_data.py babylm_10M 16 --data_dir ./babylm_data --seed 42
 

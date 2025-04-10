4da4893c3d0f9f7f58c5b86aaec234fb25cb2911
miraumlauf <bi235@uni-heidelberg.de>
2025-04-10 10:52:44 +0200

    Debug file

--- mtp_debug.sh
+++ mtp_debug.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=llama_4fh_128
-#SBATCH --nodes=1
+#SBATCH --job-name=4fh_256
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64GB
@@ -20,11 +19,14 @@ export TORCH_DISTRIBUTED_DEBUG=DETAIL
 # Run Debug 
 #python -m apps.mtp.train config=apps/mtp/configs/debug.yaml
 # torchrun --nproc-per-node=4 -m apps.mtp.train config=apps/mtp/configs/llama_lr_8e-3.yaml
-#torchrun --nproc-per-node=4 -m apps.mtp.train config=apps/mtp/llama_384/config.yaml
-torchrun --nproc-per-node=4 -m apps.mtp.train config=apps/mtp/configs/llama_4fh_128.yaml
+
+torchrun --nproc-per-node=4 -m apps.mtp.train config=apps/mtp/llama_256_4fh/config.yaml
+#torchrun --nproc-per-node=4 -m apps.mtp.train config=apps/mtp/configs/llama_4fh_128.yaml
 
 ### ntp with main transformer
-#torchrun --nproc-per-node=4 -m apps.main.train config=apps/main/configs/ntp_llama.yaml
+#torchrun --nproc-per-node=4 -m apps.main.train config=apps/main/configs/ntp_512.yaml
+# reload from model dir!!
+#torchrun --nproc-per-node=4 -m apps.main.train config=apps/main/ntp_llama_512/config.yaml
 
 
 echo "Change to config in dump dir! ✔️"

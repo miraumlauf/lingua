c0df098c65324fc4d9690ea18c0a0cd708221bef
miraumlauf <bi235@uni-heidelberg.de>
2025-04-10 10:52:06 +0200

    Updated convert files

--- convert_to_hf_sequence.py
+++ convert_to_hf_sequence.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import shutil
 from huggingface_hub import login, HfApi 
 
-from transformers import PretrainedConfig, AutoTokenizer, AutoConfig, AutoModel, PreTrainedModel, AutoModelForCausalLM
+from transformers import PretrainedConfig, AutoTokenizer, AutoConfig, AutoModel, PreTrainedModel, AutoModelForCausalLM, AutoModelForSequenceClassification
 from typing import Optional
 from omegaconf import OmegaConf
 import torch
 from apps.main.generate import load_consolidated_model_and_tokenizer
-from apps.mtp.transformer import LMTransformer, LMMTPArgs
-#from apps.main.transformer import LMTransformer, LMTransformerArgs
+#from apps.mtp.transformer import LMTransformer, LMMTPArgs
+from apps.main.transformer import LMTransformer, LMTransformerArgs
 
 from lingua.args import dump_config
 from lingua.checkpoint import CONSOLIDATE_FOLDER, consolidate_checkpoints
@@ -24,7 +24,7 @@ from lingua.distributed import (
 from huggingface.lingua_config import LinguaModelConfig
 #from huggingface.lingua_model import LinguaModel
 from huggingface.lingua_model import LinguaModelForCausalLM
-
+from huggingface.lingua_model import LinguaModelForSequenceClassification
 
 
 # registering to AutoClass for local development
@@ -33,6 +33,7 @@ AutoConfig.register("lingua_model", LinguaModelConfig)
 #AutoModel.register(LinguaModelConfig, LinguaModel) 
 # config class, model class
 AutoModelForCausalLM.register(LinguaModelConfig, LinguaModelForCausalLM)
+AutoModelForSequenceClassification.register(LinguaModelConfig, LinguaModelForSequenceClassification)
 
 
 # Directly pass your Hugging Face token
@@ -83,33 +84,24 @@ def upload(cfg: UploadArgs):
     model, tokenizer = load_consolidated_model_and_tokenizer(
         consolidate_path,
         model_cls=LMTransformer,
-        #model_args_cls=LMTransformerArgs,
-        model_args_cls=LMMTPArgs,
+        #model_args_cls=LMMTPArgs,
+        model_args_cls=LMTransformerArgs,
     )
-    logger.info("Consolidated file created")
+    logger.info("Model loaded")
 
     # ----------------------------
     # load state_dict from pth
-    # code can be used for NTP model!!!!
-    #file_path = Path("./apps/main/ntp_llama_512/checkpoints/0000015300/consolidated/consolidated.pth")
-    file_path = Path("./apps/mtp/llama_512/checkpoints/0000015300/consolidated/consolidated.pth")
-
+    file_path = Path("./apps/main/ntp_llama_128/checkpoints/0000003000/consolidated/consolidated.pth")
+    #file_path = Path("./apps/mtp/llama_128/checkpoints/0000003000/consolidated/consolidated.pth")
     state_dict = torch.load(file_path)
     model_weights = state_dict["model"]
     
-    print(model_weights.keys())
+    print("Before", model_weights.keys())
     
     for key in list(model_weights.keys()):
         model_weights[f"model.{key}"] = model_weights[key]
         del model_weights[key]
-
-
-    # ONLY FOR NTP needed
-    if "model.output.weight" in model_weights:
-        model_weights["model.heads.0.weight"] = model_weights["model.output.weight"]
-        del model_weights["model.output.weight"]
-    
-    # For strict = TRUE needed -> remove unexpected extra heads
+        
     if "model.heads.1.weight" in model_weights:
         del model_weights["model.heads.1.weight"]
     
@@ -120,85 +112,92 @@ def upload(cfg: UploadArgs):
         del model_weights["model.heads.3.weight"]
     
     
+    # FOR NTP needed
+    if "model.output.weight" in model_weights:
+        model_weights["model.heads.0.weight"] = model_weights["model.output.weight"]
+        del model_weights["model.output.weight"]
+
+    print("After", model_weights.keys())
+
     # CONFIG
+    #config = LinguaModelConfig(num_labels=2)
     config = LinguaModelConfig()
-    
 
-    
     #INITIALIZING MODELS
-    # lingua_model_128 = LinguaModel(config)
-    lingua_causal_model = LinguaModelForCausalLM(config)
+    lingua_sequence_model = LinguaModelForSequenceClassification(config)
     
-    # BASE MODEL: load weights -> only needed for only using base model
-    #lingua_model_128.load_state_dict(model_weights)
+    # SEQUENCE MODEL: load weights
+    # Ensure model_weights contains classifier weights
+    print("Model weights keys before adding classifier random weights", list(model_weights.keys()))
     
-    # ! rename output weight for CausalLM (keeping both so that I can generate AND the model has model.heads.0.weight)
-    model_weights["lm_head.weight"] = model_weights["model.heads.0.weight"]
-    del model_weights["model.heads.0.weight"]
-    print(model_weights.keys())
-    
-    # CAUSAL MODEL: load weights
-    lingua_causal_model.load_state_dict(model_weights, strict=False)
-    
-    # tok_embeddings_weight = model_weights["model.tok_embeddings.weight"]
-    # heads_0_weight = model_weights["model.heads.0.weight"]
+    if "classifier.weight" not in model_weights or "classifier.bias" not in model_weights:
+        print("Classifier weights are missing in model_weights. Adding them manually!")
 
-    print("LM Causal Model keys:", list(lingua_causal_model.state_dict().keys()))
+        model_weights["classifier.weight"] = lingua_sequence_model.state_dict()["classifier.weight"]
+        model_weights["classifier.bias"] = lingua_sequence_model.state_dict()["classifier.bias"]
+
+        print("Added classifier weights to model_weights.")
+    
+    # initializing sequence model with random weights -> can only be used with fine-tuning 
+    lingua_sequence_model.load_state_dict(model_weights, strict=True)
+    
+    print("LM Sequence Model keys:", list(lingua_sequence_model.state_dict().keys()))
+    print("Model weights keys", list(model_weights.keys()))
+    # adding model to specific classifier weights
 
     
     # try registering here
     AutoConfig.register("lingua_model", LinguaModelConfig)  
     #AutoModel.register(LinguaModelConfig, LinguaModel)
     AutoModelForCausalLM.register(LinguaModelConfig, LinguaModelForCausalLM)
+    AutoModelForSequenceClassification.register(LinguaModelConfig, LinguaModelForSequenceClassification)
     
     LinguaModelConfig.register_for_auto_class()
     #LinguaModel.register_for_auto_class("AutoModel")
     LinguaModelForCausalLM.register_for_auto_class("AutoModelForCausalLM")
+    LinguaModelForSequenceClassification.register_for_auto_class("AutoModelForSequenceClassification")
     
-  
-    # # # save CAUSAL model
-    save_dir = Path("./lingua_3fh_causal_512_15300")
-    save_dir_outside = Path("../lingua_3fh_causal_512_15300")
     
-    # create dir
-    save_dir.mkdir(parents=True, exist_ok=True)
+    # save SEQUENCE MODEL
+    #save_dir = Path("./ntp_lingua_sequence_128_3000")
+    save_dir_outside = Path("../ntp_lingua_sequence_128_3000")
+    #save_dir.mkdir(parents=True, exist_ok=True)
     save_dir_outside.mkdir(parents=True, exist_ok=True)
-    
-    # save model in created dir
-    lingua_causal_model.save_pretrained(save_dir, safe_serialization=False)
+    #lingua_model_128.save_pretrained(save_dir)
+    #lingua_sequence_model.save_pretrained(save_dir, safe_serialization=False)
     # save directly for using it in eval pipeline
-    lingua_causal_model.save_pretrained(save_dir_outside, safe_serialization=False)
-
+    lingua_sequence_model.save_pretrained(save_dir_outside, safe_serialization=False)
     
-
-    # push_to_hub not possible because of weight tie....
-    # #lingua_model_128.push_to_hub("llama_128_15300", private=True)
-    #lingua_causal_model.push_to_hub("llama_causal_128_15300", private=True, safe_serialization=False)
-
-    shutil.copy("./huggingface/__init__.py", f"{save_dir}/__init__.py")
+    #shutil.copy("./huggingface/__init__.py", f"{save_dir}/__init__.py")
     shutil.copy("./huggingface/__init__.py", f"{save_dir_outside}/__init__.py")
-
-    ## NOT PUSHING TO HF RIGHT NOW
-    # api = HfApi()
     
-    # # causal model
-    # repo_id = "umlauf/lingua_3fh_causal_512_15300"
+    ## NOT PUSHING TO HF RIGHT NOW  
+    ## push_to_hub not possible because of weight tie....
+    # api = HfApi()
+  
+    # # sequence model
+    # repo_id = "umlauf/ntp_lingua_sequence_128_3000"
 
     # api.upload_folder(
-    #     folder_path="./lingua_3fh_causal_512_15300",
+    #     folder_path="./ntp_lingua_sequence_128_3000",
     #     repo_id=repo_id,
     #     repo_type="model",
     # )
-    
+
     # ---------------------
     # TOKENIZER
     logger.info("Pushing tokenizer to HF")
     tokenizer_path = Path("./tokenizers/llama3/original")
     hf_tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-    hf_tokenizer.save_pretrained(save_dir)
+    # define pad token for 
+    if hf_tokenizer.pad_token is None:
+        hf_tokenizer.pad_token = hf_tokenizer.eos_token
+    #hf_tokenizer.save_pretrained(save_dir)
+    # add outside repo for eval pipeline
     hf_tokenizer.save_pretrained(save_dir_outside)
-    #hf_tokenizer.push_to_hub(f"lingua_3fh_causal_512_15300", private=True)
-    logger.info("Tokenizer pushed")
+    #hf_tokenizer.push_to_hub(f"ntp_lingua_sequence_128_3000", private=True)
+    #logger.info("Tokenizer pushed")
+    
     
 
 
